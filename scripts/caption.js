a08f14403a0a0294bab11d1064d0d8a0246e37d6
artyomtalonchick <artyomtalonchick@gmail.com>
2019-10-19 00:32:22 +0300

    Other case for edje

--- scripts/caption.js
+++ scripts/caption.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    var isEdge = !isIE && !!window.StyleMedia;
     var mainDiv = document.getElementById('main-div');
     var fullCaption = document.getElementById('caption');
     var svg = document.getElementById('svg-caption');
-    if(isIE){
+    if(isIE || isEdge){
         mainDiv.removeChild(svg);
     } else {
         mainDiv.removeChild(fullCaption);
